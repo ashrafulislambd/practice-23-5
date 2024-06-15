cb9d6cd16203eb1d9c74c7e2d02e51f2d453c445
Md. Ashraful Islam <imdashraful17@gmail.com>
2024-06-15 16:42:23 +0600

    First commit

---
+++ accounts/forms.py
@@ -0,0 +1,139 @@
+
+from typing import Any
+from django.contrib.auth.base_user import AbstractBaseUser
+from django.contrib.auth.forms import UserCreationForm, PasswordChangeForm
+from django import forms
+from .constants import ACCOUNT_TYPE, GENDER_TYPE
+from django.contrib.auth.models import User
+from .models import UserBankAccount, UserAddress
+
+class UserRegistrationForm(UserCreationForm):
+    birth_date = forms.DateField(widget=forms.DateInput(attrs={'type':'date'}))
+    gender = forms.ChoiceField(choices=GENDER_TYPE)
+    account_type = forms.ChoiceField(choices=ACCOUNT_TYPE)
+    street_address = forms.CharField(max_length=100)
+    city = forms.CharField(max_length= 100)
+    postal_code = forms.IntegerField()
+    country = forms.CharField(max_length=100)
+    class Meta:
+        model = User
+        fields = ['username', 'password1', 'password2', 'first_name', 'last_name', 'email', 'account_type', 'birth_date','gender', 'postal_code', 'city','country', 'street_address']
+        
+        # form.save()
+    def save(self, commit=True):
+        our_user = super().save(commit=False) # ami database e data save korbo na ekhn
+        if commit == True:
+            our_user.save() # user model e data save korlam
+            account_type = self.cleaned_data.get('account_type')
+            gender = self.cleaned_data.get('gender')
+            postal_code = self.cleaned_data.get('postal_code')
+            country = self.cleaned_data.get('country')
+            birth_date = self.cleaned_data.get('birth_date')
+            city = self.cleaned_data.get('city')
+            street_address = self.cleaned_data.get('street_address')
+            
+            UserAddress.objects.create(
+                user = our_user,
+                postal_code = postal_code,
+                country = country,
+                city = city,
+                street_address = street_address
+            )
+            UserBankAccount.objects.create(
+                user = our_user,
+                account_type  = account_type,
+                gender = gender,
+                birth_date =birth_date,
+                account_no = 100000+ our_user.id
+            )
+        return our_user
+    
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        
+        for field in self.fields:
+            self.fields[field].widget.attrs.update({
+                
+                'class' : (
+                    'appearance-none block w-full bg-gray-200 '
+                    'text-gray-700 border border-gray-200 rounded '
+                    'py-3 px-4 leading-tight focus:outline-none '
+                    'focus:bg-white focus:border-gray-500'
+                ) 
+            })
+
+
+# profile ki ki jinis update korte parbe amader user
+
+class UserUpdateForm(forms.ModelForm):
+    birth_date = forms.DateField(widget=forms.DateInput(attrs={'type':'date'}))
+    gender = forms.ChoiceField(choices=GENDER_TYPE)
+    account_type = forms.ChoiceField(choices=ACCOUNT_TYPE)
+    street_address = forms.CharField(max_length=100)
+    city = forms.CharField(max_length= 100)
+    postal_code = forms.IntegerField()
+    country = forms.CharField(max_length=100)
+
+    class Meta:
+        model = User
+        fields = ['first_name', 'last_name', 'email']
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field in self.fields:
+            self.fields[field].widget.attrs.update({
+                'class': (
+                    'appearance-none block w-full bg-gray-200 '
+                    'text-gray-700 border border-gray-200 rounded '
+                    'py-3 px-4 leading-tight focus:outline-none '
+                    'focus:bg-white focus:border-gray-500'
+                )
+            })
+        # jodi user er account thake 
+        if self.instance:
+            try:
+                user_account = self.instance.account
+                user_address = self.instance.address
+            except UserBankAccount.DoesNotExist:
+                user_account = None
+                user_address = None
+
+            if user_account:
+                self.fields['account_type'].initial = user_account.account_type
+                self.fields['gender'].initial = user_account.gender
+                self.fields['birth_date'].initial = user_account.birth_date
+                self.fields['street_address'].initial = user_address.street_address
+                self.fields['city'].initial = user_address.city
+                self.fields['postal_code'].initial = user_address.postal_code
+                self.fields['country'].initial = user_address.country
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        if commit:
+            user.save()
+
+            user_account, created = UserBankAccount.objects.get_or_create(user=user) # jodi account thake taile seta jabe user_account ar jodi account na thake taile create hobe ar seta created er moddhe jabe
+            user_address, created = UserAddress.objects.get_or_create(user=user) 
+
+            user_account.account_type = self.cleaned_data['account_type']
+            user_account.gender = self.cleaned_data['gender']
+            user_account.birth_date = self.cleaned_data['birth_date']
+            user_account.save()
+
+            user_address.street_address = self.cleaned_data['street_address']
+            user_address.city = self.cleaned_data['city']
+            user_address.postal_code = self.cleaned_data['postal_code']
+            user_address.country = self.cleaned_data['country']
+            user_address.save()
+
+        return user
+    
+class UserPasswordChangeForm(PasswordChangeForm):
+    def __init__(self, user, *args, **kwargs):
+        super().__init__(user, *args, **kwargs)
+        self.fields['old_password'].widget = forms.PasswordInput(attrs={ "class" : "p-2 w-full rounded-[7px]  !border  !border-gray-300 border-t-transparent bg-transparent bg-white px-3 py-2.5 font-sans text-sm font-normal text-blue-gray-700  shadow-lg shadow-gray-900/5 outline outline-0 ring-4 ring-transparent transition-all placeholder:text-gray-500 placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-2  focus:!border-gray-900 focus:border-t-transparent focus:!border-t-gray-900 focus:outline-0 focus:ring-gray-900/10 disabled:border-0 disabled:bg-blue-gray-50",
+                                                                        "placeholder": "Old Password" })
+        self.fields['new_password1'].widget = forms.PasswordInput(attrs={ "class" : "p-2 w-full rounded-[7px]  !border  !border-gray-300 border-t-transparent bg-transparent bg-white px-3 py-2.5 font-sans text-sm font-normal text-blue-gray-700  shadow-lg shadow-gray-900/5 outline outline-0 ring-4 ring-transparent transition-all placeholder:text-gray-500 placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-2  focus:!border-gray-900 focus:border-t-transparent focus:!border-t-gray-900 focus:outline-0 focus:ring-gray-900/10 disabled:border-0 disabled:bg-blue-gray-50",
+                                                                        "placeholder": "New Password" })
+        self.fields['new_password2'].widget = forms.PasswordInput(attrs={ "class" : "p-2 w-full rounded-[7px]  !border  !border-gray-300 border-t-transparent bg-transparent bg-white px-3 py-2.5 font-sans text-sm font-normal text-blue-gray-700  shadow-lg shadow-gray-900/5 outline outline-0 ring-4 ring-transparent transition-all placeholder:text-gray-500 placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-2  focus:!border-gray-900 focus:border-t-transparent focus:!border-t-gray-900 focus:outline-0 focus:ring-gray-900/10 disabled:border-0 disabled:bg-blue-gray-50",
+                                                                        "placeholder": "Confirm Password" })
---
+++ transactions/constants.py
@@ -0,0 +1,15 @@
+DEPOSIT = 1
+WITHDRAWAL = 2
+LOAN = 3
+LOAN_PAID = 4
+SEND = 5
+
+TRANSACTION_TYPE = (
+    (DEPOSIT, 'Deposite'),
+    (WITHDRAWAL, 'Withdrawal'),
+    (LOAN, 'Loan'),
+    (LOAN_PAID, 'Loan Paid'),
+    (SEND, 'Sent'),
+)
+
+IS_BANK_BANKRUPT = True
